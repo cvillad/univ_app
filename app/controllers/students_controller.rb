a61950c7078d9a0ab5d226a7b982a88b56717d01
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-26 11:14:54 -0500

    add show and edit students, refactor views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
   def index 
     @students = Student.all
   end
   
+  def show 
+  end
+
   def new
     @student = Student.new
   end
@@ -21,10 +25,20 @@ class StudentsController < ApplicationController
   end
 
   def update
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to @student
+    else
+      render :edit
+    end
   end
 
   private 
   def student_params
     params.require(:student).permit(:name, :email)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
